cbcb0cee3ebadc84925ec5bdd615fa47005294eb
El imali hamza <elimal.hamza@gmail.com>
2024-05-16 19:53:28 +0000

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,14 +4,10 @@ from typing import List, Tuple
 import re
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """Returns the log message obfuscated."""
-    return re.sub(r'({})=[^{}{}]+'.format('|'.join(fields),
-                                          re.escape(redaction),
-                                          re.escape(separator)),
-                  r'\1={}{}'.format(redaction,
-                                    separator),
-                  message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Return an obfuscated log message."""
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
