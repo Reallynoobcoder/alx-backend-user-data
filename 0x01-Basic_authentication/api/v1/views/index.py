522331242c5eb6bb8a0010b1edec23791dd1f10b
El imali hamza <elimal.hamza@gmail.com>
2024-05-20 19:03:17 +0000

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -20,6 +20,12 @@ def unauthorized() -> None:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def Forbidden() -> None:
+    """Error handler triggers a 403 Forbidden error."""
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
