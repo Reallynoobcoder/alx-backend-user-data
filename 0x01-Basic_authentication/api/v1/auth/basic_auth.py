b0341c46fb35013311657dccba30a518f868d9ab
El imali hamza <elimal.hamza@gmail.com>
2024-05-21 17:25:43 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Class BasicAuth that inherits from Auth."""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,17 @@ class BasicAuth(Auth):
         base64_part = authorization_header[len('Basic '):]
 
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Returns the decoded value of a Base64 string."""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_base64 = base64.b64decode(
+                base64_authorization_header, validate=True)
+            return decoded_base64.decode('utf-8')
+        except Exception:
+            return None
