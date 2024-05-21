52da1d2a58583236899b9e592f42ed360bc3aaa1
El imali hamza <elimal.hamza@gmail.com>
2024-05-21 14:58:59 +0000

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,13 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header from the request"""
-        return None
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Method to get the current user from the request"""
