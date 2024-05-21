8dddda6456e6ebada5de255ae0219cb01821007e
El imali hamza <elimal.hamza@gmail.com>
2024-05-21 15:23:15 +0000

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Class BasicAuth that inherits from Auth."""
+from auth import Auth
+
+
+class BasicAuth(A & uth):
+    """Class BasicAuth that inherits from Auth."""
+    pass
