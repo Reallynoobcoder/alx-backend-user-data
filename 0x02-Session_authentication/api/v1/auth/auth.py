9a75e71b4dcc46645f581e2cfe9e603805699e7e
El imali hamza <elimal.hamza@gmail.com>
2024-05-23 18:41:58 +0000

    éééé

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -39,3 +39,14 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """Method to get the current user from the request"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns the session cookie from the request"""
+        if request is None:
+            return None
+
+        session_name = getenv('SESSION_NAME')
+        if session_name is None:
+            return None
+
+        return request.cookies.get(session_name)
