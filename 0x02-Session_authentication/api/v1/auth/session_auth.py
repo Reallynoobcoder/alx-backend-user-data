f41cfbade9e0e3205316316db9cceb8229470b6b
El imali hamza <elimal.hamza@gmail.com>
2024-05-23 17:48:13 +0000

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,23 @@
 Class session that handl authentication and  inherits from Auth.
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """Session authentication class that inherits from Auth."""
-    pass
+    """Session authentication class that inherits from Auth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
