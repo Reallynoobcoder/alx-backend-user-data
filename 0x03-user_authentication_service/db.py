c39c0441e34a3df1866327823371e38cdc89f389
El imali hamza <elimal.hamza@gmail.com>
2024-05-29 11:57:41 +0000

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -43,10 +44,7 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """This method takes in arbitrary keyword arguments
-            and returns the first row found in the users table
-            as filtered by the method’s input arguments.
-        """
+        """Find a user by arbitrary keyword arguments."""
         if not kwargs:
             raise InvalidRequestError("No query arguments provided.")
 
