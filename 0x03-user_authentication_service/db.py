cf086d1d5bc587f1aa42b6d5262bec6272a6ff5b
El imali hamza <elimal.hamza@gmail.com>
2024-05-30 17:28:34 +0000

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,6 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
