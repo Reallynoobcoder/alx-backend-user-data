95880a74f28774bc6ec2a14e0b80d3104d3f1b4f
El imali hamza <elimal.hamza@gmail.com>
2024-05-30 17:22:23 +0000

    .

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,17 +7,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -25,34 +22,34 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """Add a new user to the database."""
+
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database based on arbitrary keyword arguments.
-        """
+        """Find a user by arbitrary keyword arguments"""
         if not kwargs:
             raise InvalidRequestError("No query arguments provided.")
 
-        user = self._session.query(User).filter_by(**kwargs).first()
+        user = self.__session.query(User).filter_by(**kwargs).first()
+
         if not user:
             raise NoResultFound("No user found matching the query.")
         return user
 
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user's attributes in the database.
+    def update_user(self, user_id: int, **kwargs: dict) -> None:
+        """
+        Update a user's attributes based on keyword arguments.
         """
         user = self.find_user_by(id=user_id)
 
