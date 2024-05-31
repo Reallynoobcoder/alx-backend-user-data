76087c280900d49888719cc1b01c57b5f1befc2b
El imali hamza <elimal.hamza@gmail.com>
2024-05-31 10:23:01 +0000

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 """Flask app."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def main():
-    """a basic Flask app that returns a jsonify message."""
+    """A basic Flask app that returns a jsonify message."""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """End-point to register a user."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
