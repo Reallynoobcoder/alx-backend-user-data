794b1a4894f2226aae4ba1787a90053d37b0ed9d
El imali hamza <elimal.hamza@gmail.com>
2024-05-31 16:49:44 +0000

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,3 +37,12 @@ class Auth:
         user = self._db.add_user(email=email, hashed_password=hashed_password)
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Valide login."""
+        try:
+            self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), User.hashed_password):
+                return True
+        except NoResultFound:
+            return False
