31abec9deaa90baf3a1053b3fdf4d0742c16c910
El imali hamza <elimal.hamza@gmail.com>
2024-06-01 18:53:47 +0000

    reset roken

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError
-        
+
         new_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=new_token)
         return new_token
