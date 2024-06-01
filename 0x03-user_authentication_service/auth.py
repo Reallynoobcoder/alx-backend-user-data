6eec205cdd1e3580e70a3cac9de4daa7356fb222
El imali hamza <elimal.hamza@gmail.com>
2024-06-01 17:44:07 +0000

    .

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,19 +69,3 @@ class Auth:
             return new_uuid
         except NoResultFound:
             return None
-
-    def get_user_from_session_id(
-            self, session_id: Optional[str]) -> Optional[User]:
-        """
-        Find a user by session ID.
-        Return: User or None: The user if found, None otherwise.
-        """
-        if session_id is None:
-            return None
-
-        try:
-            user = self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            return None
-
-        return user
