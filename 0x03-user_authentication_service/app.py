7639269ef05e4103c39a06a06fb182218c55f7a0
El imali hamza <elimal.hamza@gmail.com>
2024-06-01 18:22:15 +0000

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,17 @@ def logout() -> Response:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> Response:
+    """Endpoint to get user profile based on session_id cookie."""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
