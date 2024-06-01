65ee3654e5eecacae664427de32b63a6d246e896
El imali hamza <elimal.hamza@gmail.com>
2024-06-01 12:09:09 +0000

    .

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,3 +24,24 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Log in end point."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if Auth.valid_login(email, password):
+        session_id = Auth.create_session(email)
+        response = make_response(
+            jsonify({"email": email, "message": "logged in"}))
+        response.set_cookie("session_id", session_id)
+        return response
+
+    else:
+        return abort(401)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
