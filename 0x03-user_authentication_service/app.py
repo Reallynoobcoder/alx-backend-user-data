ad7697cee658497b228c175935ff1478802514de
El imali hamza <elimal.hamza@gmail.com>
2024-06-02 12:06:05 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,12 +89,11 @@ def update_password() -> Response:
     new_password = request.form.get("new_password")
 
     try:
-        user = AUTH._db.find_user_by(email=email)
         AUTH.update_password(reset_token, new_password)
-        return jsonify(
-            {"email": user.email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+    return jsonify(
+        {"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
